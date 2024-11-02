59aac4f1b3ffc5bfe20f5f9b16e9ef5af7ba0246
Richard <richard.s050804@gmail.com>
2024-11-02 19:34:57 +0700

    refactor: change GORM to sqlx

--- apps/backend/database/database.go
+++ apps/backend/database/database.go
@@ -1,20 +1,38 @@
 package database
 
 import (
-	"log"
+	// "database/sql"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	// "fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
-var DB *gorm.DB
+var DB *sqlx.DB
 
 func InitDB() {
-	dsn := "host=localhost user=postgres password=admin dbname=mpl port=5432 sslmode=disable TimeZone=Asia/Jakarta"
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Memuat file .env
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Mengambil DSN dari environment variable
+    dsn := os.Getenv("DB_DSN")
+    if dsn == "" {
+        log.Fatal("DB_DSN tidak ditemukan dalam .env")
+    }
+
+	DB, err = sqlx.Connect("postgres", dsn)
 
 	if err != nil {
 		log.Fatalf("failed to connect to the database: %v", err)
 	}
+
+	DB.SetMaxOpenConns(25)
+	DB.SetMaxIdleConns(5)
 }
--- apps/backend/models/team.go
+++ apps/backend/models/team.go
@@ -2,20 +2,21 @@ package models
 
 import (
 	"example.com/mpl/database"
+	"github.com/jmoiron/sqlx"
 )
 
 type Team struct {
-	ID   int    `json:"id" gorm:"primaryKey"`
-	Name string `json:"name"`
+	ID   int    `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
 }
 
 func GetAllTeams() ([]Team, error) {
 	// Get all teams from the database
 	var teams []Team
-	result := database.DB.Find(&teams)
-
-	if result.Error != nil {
-		return nil, result.Error
+	query := "SELECT id, name FROM teams"
+	err := sqlx.Select(database.DB, &teams, query)
+	if err != nil {
+		return nil, err
 	}
 	return teams, nil
 }
