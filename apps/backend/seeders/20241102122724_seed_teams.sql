4c3c028f1b9f1d511bca2e330cec3fcb6093efb2
Richard <richard.s050804@gmail.com>
2024-11-03 17:16:09 +0700

    chore : add id for seeder

--- apps/backend/seeders/20241102122724_seed_teams.sql
+++ apps/backend/seeders/20241102122724_seed_teams.sql
@@ -1,11 +1,20 @@
 -- +goose Up
 -- +goose StatementBegin
-INSERT INTO teams (name) VALUES ('EVOS'), ('FNOC'), ('RRQ'), ('TLID'), ('GEEK'), ('RBL'), ('DEWA'), ('AE'), ('BTR');
+INSERT INTO teams (id, name) VALUES 
+(1, 'AE'), 
+(2, 'BTR'), 
+(3, 'DEWA'), 
+(4, 'EVOS'),
+(5, 'FNOC'), 
+(6, 'GEEK'), 
+(7, 'RBL'), 
+(8, 'RRQ'), 
+(9, 'TLID');
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DELETE FROM teams WHERE name IN ('EVOS'), ('FNOC'), ('RRQ'), ('TLID'), ('GEEK'), ('RBL'), ('DEWA'), ('AE'), ('BTR');
+DELETE FROM teams;
 -- +goose StatementEnd
 
 -- goose -dir seeders postgres "postgres://postgres:admin@localhost:5432/mpl?sslmode=disable" up
