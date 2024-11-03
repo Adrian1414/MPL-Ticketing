7509107f3cbf23b653f86f7000387f00feec6921
Richard <richard.s050804@gmail.com>
2024-11-03 17:15:36 +0700

    chore: change id type

--- apps/backend/migrations/20241102122250_create_teams_table.sql
+++ apps/backend/migrations/20241102122250_create_teams_table.sql
@@ -1,14 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE teams (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS teams(
+    id INTEGER PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE teams;
+DROP TABLE IF EXISTS teams;
 -- +goose StatementEnd
 
 -- goose -dir migrations postgres "postgres://postgres:admin@localhost:5432/mpl?sslmode=disable" up
